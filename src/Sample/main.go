2ee417f29d62fee703aaaef282a952348557b768
Alicja Ciura <alicja.ciura@capgemini.com>
2020-11-22 20:48:19 +0100

    Add html site

--- src/Sample/main.go
+++ src/Sample/main.go
@@ -1,10 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		f, err := os.Open("public" + r.URL.Path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+		}
+		defer f.Close()
+		var contentType string
+		switch {
+		case strings.HasSuffix(r.URL.Path, "css"):
+			contentType = "text/css"
+		case strings.HasSuffix(r.URL.Path, "html"):
+			contentType = "text/html"
+		case strings.HasSuffix(r.URL.Path, "png"):
+			contentType = "image/png"
+		default:
+			contentType = "text/html"
+		}
+		w.Header().Add("Content-Type", contentType)
+		io.Copy(w, f)
+
 	})
+	http.NotFoundHandler()
 	http.ListenAndServe(":8080", nil)
 }
